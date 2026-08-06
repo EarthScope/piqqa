aa9a5f19e167e5950d5370990029574df185aa54
Laura Keyson <laura.keyson@earthscope.org>
2026-08-06 08:31:52 -0700

    fix typo

--- PIQQA.py
+++ PIQQA.py
@@ -1796,7 +1796,7 @@ def doReport(
     ###### INTRO ######
     introText = (
         f"This report is intended as a quick, broad overview of the quality of the data archived for the network "
-        f"and time period specified above. EarthScope' goal in generating these reports is to give PIs for temporary "
+        f"and time period specified above. EarthScope's goal in generating these reports is to give PIs for temporary "
         f"experiments better insight into the quality of their data archived at the DMC, as well as to demonstrate "
         f'the utility provided by the EarthScope quality assurance system <a href="http://service.earthscope.org/mustang/?" target="_blank">MUSTANG</a> '
         f"and the many metrics and products it generates. "
